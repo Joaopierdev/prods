3a0b8fe14cec55f5fb5b4372a147e6c59c8c943b
João Gabriel <127800063+Joaopierdev@users.noreply.github.com>
2024-05-16 21:12:13 -0300

    Adicionando página de cadastro de produto

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CategoriasView from '../views/CategoriasView.vue'
+import CadastroProdutos from '../views/CadastrarProduto.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/categorias',
       name: 'categorias',
       component: CategoriasView
+    },
+    {
+      path: '/produtos/cadastrar',
+      name: 'cadastrar produtos',
+      component: CadastroProdutos
     }
   ]
 })
