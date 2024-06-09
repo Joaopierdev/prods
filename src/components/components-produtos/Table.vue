598aec0d8349e0fa1f53615e92df9797a3fa0a68
João Gabriel <127800063+Joaopierdev@users.noreply.github.com>
2024-06-09 19:52:25 -0300

    Alterando informações de filtro

--- src/components/components-produtos/Table.vue
+++ src/components/components-produtos/Table.vue
@@ -32,7 +32,52 @@ function showModalRemove() {
 <template>
     <main class="content">
         <class class="filter-desktop">
-            <Filter />
+            <main class="content border-solid border-2 border-gray-200 shadow-lg rounded-lg">
+                <div class="overflow-x-auto">
+                    <form class="filter flex flex-col lg:flex-row sm:space-x-4 max-w-sm mx-auto my-4">
+                        <div class="flex flex-col w-full sm:w-1/2">
+                            <label for="number-input"
+                                class="block mb-0 text-sm font-medium text-gray-900 dark:text-white">Código:</label>
+                            <input type="number" id="number-input" aria-describedby="helper-text-explanation"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="12345" />
+                        </div>
+                        <div class="flex flex-col w-full sm:w-1/2">
+                            <label for="default-input" class="block mb-0 text-sm font-medium text-gray-900 dark:text-white">Nome:</label>
+                            <input type="text" id="default-input"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="Nome do produto">
+                        </div>
+                        <div class="flex flex-col w-full sm:w-1/2">
+                            <label for="status" class="block text-sm font-medium text-gray-900 dark:text-white">
+                                Status
+                            </label>
+                            <select id="status"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                <option>Ativo</option>
+                                <option>Inativo</option>
+                            </select>
+                        </div>
+
+                        <div class="flex flex-col w-full sm:w-1/2">
+                            <label for="status" class="block text-sm font-medium text-gray-900 dark:text-white">
+                                Categoria
+                            </label>
+                            <select id="status"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                <option selected>Selecione</option>
+                                <ListaCategorias />
+                            </select>
+                        </div>
+                        <div class="flex flex-col sm:flex-row justify-between items-center">
+                            <button
+                            
+                                class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                                Filtrar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </main>
         </class>
         <class class="filter-mobile">
             <FilterMobile />
